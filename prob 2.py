9f3a7c9259f7a284c00c57d365ac3751cd6b2422
Băbuță Luminița <70962140+hopeniitssaa@users.noreply.github.com>
2022-09-14 11:07:50 +0300

    Add files via upload

---
+++ prob 2.py
@@ -0,0 +1,7 @@
+x = int(input("Dati x: "))
+y = int(input("Dati y: "))
+z = int(input("Dati z: "))
+q = int(input("Dati q: "))
+
+def fractionOperations():
+    print(f'Suma este {((x*q) + (y*z)) / (y*q)}, iar produsul este {(x*z) / (y*q)}')
---
+++ prob1.py
@@ -0,0 +1,3 @@
+def putere(x,y):
+    return x**y
+print('Valoarea expresiei este ', 1+ putere(0.5, 2)+ putere(0.5, 4)+ putere(0.5,6))
